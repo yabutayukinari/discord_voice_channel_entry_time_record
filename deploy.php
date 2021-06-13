b4047f8a70410207826473e24919c144e625e3a6
y.y <yabutayukinari@gmail.com>
2021-06-13 17:56:53 +0900

    github actions 追加

---
+++ deploy.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Deployer;
+
+require_once 'recipe/common.php';
+
+// Project repository
+set('repository', 'git@github.com:digmee/missing-message-fixer.git');
+
+host(getenv('IP_ADDRESS'))
+    ->port(getenv('PORT_NO'))
+    ->user(getenv('USER_NAME'))
+    ->identityFile('~/.ssh/id_rsa')
+    ->addSshOption('StrictHostKeyChecking', 'no')
+    ->stage('staging')
+    ->set('deploy_path', '/var/bot/type77_test');
+
+set('shared_dirs', []);
+set('shared_files', ['.env', 'src/services.php', 'mybot.sqlite3']);
+set('writable_dirs', []);
+set('writable_mode', 'chmod');
+set('writable_chmod_mode', '0777');
+set('writable_use_sudo', true);
+
+desc('Deploy project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
+
+after('deploy', 'success');
+after('deploy:failed', 'deploy:unlock');
